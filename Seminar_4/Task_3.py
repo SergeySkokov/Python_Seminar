80989a885f4faee50b69f2c1be59585b98ba8402
SergeySkokov <skokovs@inbox.ru>
2022-09-18 22:09:09 +0400

    Homework for Seminar 4

---
+++ Seminar_4/Task_3.py
@@ -0,0 +1,16 @@
+# Задайте последовательность чисел. Напишите программу, которая выведет список
+# неповторяющихся элементов исходной последовательности.
+# [1, 1, 2, 3, 4, 5, 5] -> [2, 3, 4]
+
+numbers = list(map(int, input("Введите числа через пробел:\n").split()))
+
+def get_unique_numbers(numbers):
+    list_of_unique_numbers = []
+    unique_numbers = set(numbers)
+
+    for number in unique_numbers:
+        list_of_unique_numbers.append(number)
+
+    return list_of_unique_numbers
+
+print(get_unique_numbers(numbers))
